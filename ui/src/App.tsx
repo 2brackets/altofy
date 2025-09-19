533c3d03cc2db3479f4c76dd41947e72724893b0
2brackets <h.skogsberg89@gmail.com>
2025-09-19 15:04:21 +0200

    add a navbar

---
+++ ui/src/App.tsx
@@ -0,0 +1,33 @@
+import Navbar from "./components/Navbar";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <div className="avatar avatar-placeholder">
+  <div className="bg-neutral text-neutral-content w-24 rounded-full">
+    <span className="text-3xl">D</span>
+  </div>
+</div>
+<div className="avatar avatar-online avatar-placeholder">
+  <div className="bg-neutral text-neutral-content w-16 rounded-full">
+    <span className="text-xl">AI</span>
+  </div>
+</div>
+<div className="avatar avatar-placeholder">
+  <div className="bg-neutral text-neutral-content w-12 rounded-full">
+    <span>SY</span>
+  </div>
+</div>
+<div className="avatar avatar-placeholder">
+  <div className="bg-neutral text-neutral-content w-8 rounded-full">
+    <span className="text-xs">UI</span>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
